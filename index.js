5d9b70490c3c9f74cc0b0ca5b99d7244813be0ce
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2017-10-21 11:46:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@
 'use strict';
 var Alexa = require("alexa-sdk");
 var APP_ID = 'amzn1.ask.skill.31ff6063-80cb-4b93-935f-a7f7761facf2';//enter app Id here 
-
+var date = "9/10/17";
+var projectNum = "432784237"
+var Size = "50"
+//Put more vars here 
 var languageStrings = {
     "en": {
         "translation": {
@@ -32,6 +35,10 @@ var handlers = {
         this.emit(':ask', "Welcome to Revit Finder, Say your filename, and I will tell you infomation about it");
         this.emit('getFile');//goes to the get file
     },
+	'FileList': function() {
+        this.emit(':ask', "Here are some of your files" + " ........... Test.RVT" + " ........ Project.rvt"+ " ......... Construction.rvt");
+        //this.emit(':ask','Test.rvt <break time="1s"/> Apple <break time="1s"/> Project.RVT');//goes to the get file
+    },
 	
     'GetFile': function() {
         var fileGet = this.event.request.intent.slots.filename.value;
@@ -39,9 +46,15 @@ var handlers = {
         if (fileGet == null || fileGet === "undefined" || fileGet == '') { //Alexa doesnt understand the word, so respond with IDK.
             this.emit('Unhandled'); //send to unhandled handler
         } else {
-            this.emit(':ask',"your file is "+fileGet);
+            this.emit(':ask',"your file is ..... "+fileGet + ".....Size is " + Size +".........Date last accessed is....... " + date + " Project Number is ......" + projectNum );
+			this.emit('FileInfo');
         // File server function logic goes here!!!
     }},
+	'FileInfo': function() {
+        SIZE = "50"
+		this.emit(':ask', "Here are some of the info on that file")
+        this.emit("Filesize is "+ SIZE);
+    },
     'AMAZON.HelpIntent': function() {
         var speechOutput = this.t("HELP_MESSAGE");
         var reprompt = this.t("HELP_REPROMPT");
